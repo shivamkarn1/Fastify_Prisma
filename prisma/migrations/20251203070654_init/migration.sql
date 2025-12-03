8a105395836fef6c057e1cb4199c14cd1cce8e93
shivamkarn <shivamkarn767@gmail.com>
2025-12-03 12:54:37 +0545

    feat: add PostgreSQL support with Prisma integration
    - Updated package.json to include Prisma and PostgreSQL dependencies.
    - Created prisma.config.ts for Prisma configuration.
    - Added initial migration for User and Product tables with necessary constraints and indexes.
    - Defined Prisma schema with User and Product models.
    - Included migration lock file for version control.

---
+++ prisma/migrations/20251203070654_init/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "content" TEXT,
+    "price" DECIMAL(10,2) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "ownerId" INTEGER NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_email_idx" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "Product_ownerId_idx" ON "Product"("ownerId");
+
+-- CreateIndex
+CREATE INDEX "Product_title_idx" ON "Product"("title");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
