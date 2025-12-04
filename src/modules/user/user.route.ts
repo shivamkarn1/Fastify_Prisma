26ec37639d8f248c5ecb7797dffd09432f0b74fa
shivamkarn <shivamkarn767@gmail.com>
2025-12-04 15:13:44 +0545

    Routes setup for user

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,7 +1,40 @@
 import type { FastifyInstance } from "fastify";
-import { registerUser } from "./user.controller";
+import { registerUser, loginUser, getUsers } from "./user.controller";
+import { userSchemaRef as $ref } from "./user.schema";
 
 async function userRoutes(app: FastifyInstance) {
-  app.post("/create", registerUser);
+  app.post(
+    "/create",
+    {
+      schema: {
+        body: $ref("createUserSchema"),
+        response: {
+          201: $ref("createUserResponseSchema"),
+        },
+      },
+    },
+    registerUser
+  );
+
+  app.post(
+    "/login",
+    {
+      schema: {
+        body: $ref("loginSchema"),
+        response: {
+          200: $ref("loginResponseSchema"),
+        },
+      },
+    },
+    loginUser
+  );
+
+  app.get(
+    "/",
+    {
+      preHandler: [app.authenticate],
+    },
+    getUsers
+  );
 }
 export { userRoutes };
