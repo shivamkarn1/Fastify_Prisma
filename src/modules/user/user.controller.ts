798bdc25ece77b409dfbee9908864ffd5b32456b
shivamkarn <shivamkarn767@gmail.com>
2025-12-04 15:13:36 +0545

    User controller added

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,67 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
-
-async function registerUser(request: FastifyRequest, reply: FastifyReply) {
-  return { status: "Created Successfully" };
+import { createUserSchema, type CreateUserInput } from "./user.schema";
+import { createUser, findUserByEmail, findUsers } from "./user.service";
+import type { loginInput } from "./user.schema";
+import { verifyPassword } from "../../utils/hash";
+import { app } from "../../app";
+async function registerUser(
+  request: FastifyRequest<{
+    Body: CreateUserInput;
+  }>,
+  reply: FastifyReply
+) {
+  try {
+    const body = request.body;
+    const user = await createUser(body);
+    return reply.status(201).send(user);
+  } catch (error) {
+    console.log(error);
+    return reply.code(500).send({ error: "Internal Server Error" });
+  }
 }
 
-export { registerUser };
+async function loginUser(
+  request: FastifyRequest<{
+    Body: loginInput;
+  }>,
+  reply: FastifyReply
+) {
+  const body = request.body;
+
+  // find user by email
+  const user = await findUserByEmail(body.email);
+  if (!user) {
+    return reply.code(401).send({
+      message: "Invalid email or password",
+    });
+  }
+
+  // verify password
+  const correctPassword = verifyPassword({
+    candidatePassword: body.password,
+    salt: user.salt,
+    hash: user.password,
+  });
+
+  if (!correctPassword) {
+    return reply.code(401).send({
+      message: "Invalid Email or Password",
+    });
+  }
+  // generate/give access token
+  const { password, salt, ...rest } = user;
+  return {
+    accessToken: app.jwt.sign(rest),
+  };
+
+  // resposne
+  return reply.code(200).send({
+    message: "User Login success",
+  });
+}
+
+async function getUsers() {
+  const users = await findUsers();
+}
+
+export { registerUser, loginUser, getUsers };
