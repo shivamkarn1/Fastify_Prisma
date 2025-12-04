38ccd613b8ca70603d2f680ca6c10ddc31b40683
shivamkarn <shivamkarn767@gmail.com>
2025-12-04 15:14:49 +0545

    Completed app functionality

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,73 @@
-import Fastify from "fastify";
+import Fastify, { type FastifyReply, type FastifyRequest } from "fastify";
+import fjwt from "@fastify/jwt";
 import { userRoutes } from "./modules/user/user.route";
-const app = Fastify();
+import { userSchemas } from "./modules/user/user.schema";
+import { productSchemas } from "./modules/products/product.schema";
+import productRoutes from "./modules/products/product.route";
+import swagger from "@fastify/swagger";
+import swaggerUI from "@fastify/swagger-ui";
+import { withRefResolver } from "fastify-zod";
+export const app = Fastify();
+
+declare module "fastify" {
+  export interface FastifyInstance {
+    authenticate: any;
+  }
+}
+
+declare module "@fastify/jwt" {
+  interface FastifyJWT {
+    user: {
+      id: number;
+      email: string;
+      name: string;
+    };
+  }
+}
+
+// Register JWT plugin
+app.register(fjwt, {
+  secret: process.env.JWT_SECRET || "your-secret-key-here",
+});
+
+app.decorate(
+  "authenticate",
+  async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      await request.jwtVerify();
+    } catch (error) {
+      return reply.send(error);
+    }
+  }
+);
 
 app.get("/health", async function () {
   return { status: "OK" };
 });
 
 async function main() {
+  for (const schema of [...userSchemas, ...productSchemas]) {
+    app.addSchema(schema);
+  }
+  app.register(
+    swagger,
+    withRefResolver({
+      openapi: {
+        info: {
+          title: "Fastify API",
+          description: "API for some products",
+          version: "1.0.0",
+        },
+      },
+    })
+  );
+
+  app.register(swaggerUI, {
+    routePrefix: "/docs",
+  });
+
   app.register(userRoutes, { prefix: "/api/users" });
+  app.register(productRoutes, { prefix: "/api/products" });
   const port = 4000;
   try {
     await app.listen({ port, host: "0.0.0.0" });
